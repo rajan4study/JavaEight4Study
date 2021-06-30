eb9ec2134142e229cf0d66ccf27adc2f627a2e50
rajan4Study <rajan943@gmail.com>
2021-06-30 16:42:29 +0530

    added file for string operation

---
+++ src/com/study/java/string/StringCompOp.java
@@ -0,0 +1,26 @@
+package com.study.java.string;
+
+/**
+ * @author rajan
+ * String compare oprations- ==.equals(),compareTo
+ *
+ */
+public class StringCompOp {
+
+	public static void main(String[] args) {
+		String s1="rajan";
+		s1.concat("m");
+		String s2="rajan";
+		System.out.println(s1);
+		String s3= new String("rajan");
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s3));
+		System.out.println(s1==s3);
+		System.out.println(s1.equals(s3));
+		String s4="abs";
+		System.out.println(s4.compareTo(s1));
+		
+
+	}
+
+}
